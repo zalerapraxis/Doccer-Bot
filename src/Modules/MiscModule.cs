ebd6df5022655dc569a2e52d2bf215b7fcd11484
Zalera <cjpff14@gmail.com>
2019-05-08 22:30:52 -0400

    delay after delete to hopefully avoid ratelimiting

--- src/Modules/MiscModule.cs
+++ src/Modules/MiscModule.cs
@@ -30,6 +30,8 @@ namespace Doccer_Bot.Modules
 
             // delete the command message
             await commandMessage.DeleteAsync();
+            // wait a second to avoid ratelimiting
+            await Task.Delay(1000);
             // make a new message with the contents of the command message
             var responseMsg = await ReplyAsync(messageContents);
             // add our vote emojis to the message
