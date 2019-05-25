0ce69e6a16b5a82a6067c80bb8f745009bff64cf
Zalera <cjpff14@gmail.com>
2019-05-25 16:38:23 -0400

    (untested) support deleting msgs older than 2 weeks, add cmd examples

--- src/Modules/ChatClearingModule.cs
+++ src/Modules/ChatClearingModule.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Linq;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using System.Threading.Tasks;
+using Doccer_Bot.Modules.Common;
 using Doccer_Bot.Services;
 
 namespace Example.Modules
@@ -17,12 +19,17 @@ namespace Example.Modules
 
         // clear chatlogs
         [Command("clear")]
-        [Summary("Clears the last x messages in current channel - clear (number) - leaving blank will clear last 100 messages")]
+        [Summary("Clears the last x messages in current channel - default 100")]
+        [Example("clear 100")]
+        [RequireBotPermission(ChannelPermission.ManageMessages)]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ClearChatlogsAsync(int count = 100)
         {
             var channel = Context.Channel as SocketTextChannel;
             var messages = await channel.GetMessagesAsync(count + 1).FlattenAsync();
+
+            // get messages older than two weeks, which cannot be bulk-deleted
+            var oldMessages = messages.Where(msg => msg.Timestamp.AddDays(14) < DateTimeOffset.Now);
 
             var server = Servers.ServerList.Find(x => x.DiscordServer == Context.Guild);
 
@@ -30,7 +37,23 @@ namespace Example.Modules
                 // remove schedule embed message from the messages list, so it doesn't get deleted
                 messages = messages.Where(msg => msg.Id != server.EventEmbedMessage.Id);
 
-            await channel.DeleteMessagesAsync(messages);
+            try
+            {
+                // bulk delete messages - only works on messages less than two weeks old
+                await channel.DeleteMessagesAsync(messages);
+            }
+            catch
+            {
+                if (oldMessages.Any())
+                {
+                    // individually delete messages two weeks or older
+                    foreach (var oldMessage in oldMessages)
+                    {
+                        await oldMessage.DeleteAsync();
+                        await Task.Delay(250);
+                    }
+                }
+            }
         }
     }
 }
