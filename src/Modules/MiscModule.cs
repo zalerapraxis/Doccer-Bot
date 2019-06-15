7f81199aaaac360339db1fb8c2b647b8fa8f0b07
Zalera <cjpff14@gmail.com>
2019-06-15 19:33:49 -0400

    move meme stuff to its own module

--- src/Modules/MiscModule.cs
+++ src/Modules/MiscModule.cs
@@ -13,9 +13,6 @@ namespace Doccer_Bot.Modules
     [Name("Misc")]
     public class MiscModule : InteractiveBase
     {
-        public DatabaseService DatabaseService { get; set; }
-
-
         [Command("vote", RunMode = RunMode.Async)]
         [Summary("Start a vote using yes/no reactions")]
         [Example("vote {message}")]
@@ -45,47 +42,11 @@ namespace Doccer_Bot.Modules
             await responseMsg.AddReactionsAsync(emotes);
         }
 
-
-        [Command("meme", RunMode = RunMode.Async)]
-        [Summary("Posts a copypasta - returns a list of all stored copypasta if left blank")]
-        [Example("meme {memename}")]
-        public async Task PostMemeCommandAsync([Remainder]string query = null)
+        [Command("doccer")]
+        [Summary("Say hi")]
+        public async Task Doccer()
         {
-            var memes = await DatabaseService.GetTextMemes();
-
-            if (query != null)
-            {
-                var response = memes.Find(x => x.Name == query);
-                await ReplyAsync(response.Text);
-            }
-            else
-            {
-                EmbedBuilder embedBuilder = new EmbedBuilder();
-                // these each represent a a field in the embed - they map out to columns due to the inline param of addfield
-                StringBuilder sBuilder1 = new StringBuilder();
-                StringBuilder sBuilder2 = new StringBuilder();
-
-                // sort the results into two columns for displaying in embed
-                var i = 0;
-                foreach (var meme in memes)
-                {
-                    // 0 for left column, 1 for right, 2 resets to 0
-                    // creates two strings containing alternating lists of meme names from memes list
-                    if (i == 2)
-                        i = 0;
-                    if (i == 0)
-                        sBuilder1.AppendLine(meme.Name);
-                    if (i == 1)
-                        sBuilder2.AppendLine(meme.Name);
-
-                    i++;
-                }
-
-                embedBuilder.AddField("Options", sBuilder1.ToString(), true);
-                embedBuilder.AddField("\u200b", sBuilder2.ToString(), true); // name string here is a zero-width space
-
-                await ReplyAsync(null, false, embedBuilder.Build());
-            }
+            await ReplyAsync("Hi!");
         }
     }
 }
